79683a197c8beff8585c75f71e02a41b699f8628
JinHwan Jeon <jinhwan0014@gmail.com>
2022-06-11 00:11:29 +0900

    Update CircleFitV2.m

--- CircleFitV2.m
+++ CircleFitV2.m
@@ -1,4 +1,4 @@
-function [x, y, R, delL, error, res] = CircleFitV2(X_l, X_r, Y_l, Y_r, w_l, w_r, plot_flag)
+function [x, y, R, delL, theta, error] = CircleFitV2(X_l, X_r, Y_l, Y_r, w_l, w_r, plot_flag)
 %% Circular Fitting for 2 Parallel Lanes
 % Upgraded version from CircleFit for 2 lanes
 % Since 2 lanes are parallel, they should share the same circle
@@ -20,8 +20,14 @@ function [x, y, R, delL, error, res] = CircleFitV2(X_l, X_r, Y_l, Y_r, w_l, w_r,
 % R: radius of optimized circle, strictly positive
 % delL: lane spacing value for 2 lanes, may vary in sign depending on the
 % location of center point of the circle
+% theta: effective center circle angle
 % error: fitting error information    
-    res = struct();
+    
+    % Data shifting to minimize scaling error 
+    X_mean = mean([X_l; X_r]);
+    Y_mean = mean([Y_l; Y_r]);
+    X_l = X_l - X_mean; X_r = X_r - X_mean;
+    Y_l = Y_l - Y_mean; Y_r = Y_r - Y_mean;
 
     A = zeros(4,4);
     b = zeros(4,1);
@@ -64,41 +70,88 @@ function [x, y, R, delL, error, res] = CircleFitV2(X_l, X_r, Y_l, Y_r, w_l, w_r,
            sum(w_r.* X_r.^2) + sum(w_r.* Y_r.^2);
 
     opt = A \ b;
-    res.A = A; res.b = b;
 
-    x = 1/2 * opt(1);
-    y = 1/2 * opt(2);
+    x_rel = 1/2 * opt(1);
+    y_rel = 1/2 * opt(2);
     C = opt(3); D = opt(4);
     
-    % D > 0 ==> Circle is on the left
-    % D < 0 ==> Circle is on the right
+    % D > 0 ==> Circle is on the left lane region
+    % D < 0 ==> Circle is on the right lane region
 
-    RpdL = sqrt(x^2 + y^2 + C + D);
+    RpdL = sqrt(x_rel^2 + y_rel^2 + C + D);
     
+    x = x_rel + X_mean;
+    y = y_rel + Y_mean;
+
     R = RpdL/2 + sqrt((RpdL/2)^2 - D/2);
     delL = RpdL/2 - sqrt((RpdL/2)^2 - D/2);
-
+    
     if ~isreal(RpdL)
         warning('Incompatible results: adjust the sampling regions')
     end
+
+    X_l = X_l + X_mean; X_r = X_r + X_mean;
+    Y_l = Y_l + Y_mean; Y_r = Y_r + Y_mean;
+    
+    %% Compute Effective Angle 
+    th = 0:pi/10000:2*pi;
+    xf1 = (R - delL) * cos(th) + x;
+    yf1 = (R - delL) * sin(th) + y;
+    
+    % Find approximate angle first and search around that angle
+    d1 = (xf1 - X_l(1)).^2 + (yf1 - Y_l(1)).^2;
+    d2 = (xf1 - X_l(end)).^2 + (yf1 - Y_l(end)).^2;
+
+    [~,idx1] = min(d1);
+    [~,idx2] = min(d2);
+    mean_th = 1/2*(th(idx1) + th(idx2));
+    delta = max(th(idx1),th(idx2)) - mean_th + 0.1;
+
+    th = linspace(mean_th - delta, mean_th + delta, 1e5);
+    
+    xf1 = (R - delL) * cos(th) + x;
+    yf1 = (R - delL) * sin(th) + y;
+
+    xf2 = (R + delL) * cos(th) + x;
+    yf2 = (R + delL) * sin(th) + y;
+
+    th_list_l = zeros(1,length(X_l));
+    th_list_r = th_list_l;
+
+    for i=1:length(X_l)
+        d_l = (xf1 - X_l(i)).^2 + (yf1 - Y_l(i)).^2;
+        d_r = (xf2 - X_r(i)).^2 + (yf2 - Y_r(i)).^2;
+
+        [~,idx_l] = min(d_l); [~,idx_r] = min(d_r);
+        th_list_l(i) = th(idx_l); 
+        th_list_r(i) = th(idx_r);
+    end
+    
+    theta = struct();
+    theta.lb = min([th_list_l th_list_r]); theta.ub = max([th_list_l th_list_r]);
+    theta.ang = theta.ub - theta.lb; 
+
+    th_lbidx = find(th == theta.lb);
+    th_ubidx = find(th == theta.ub);
+
     %% Plot Results
     if plot_flag
         figure(1);
-        p_data = plot(X_l, Y_l, 'ro'); hold on; grid on; axis equal; 
-        plot(X_r, Y_r, 'bo');
+        p_data_l = plot(X_l, Y_l, 'r.'); hold on; grid on; axis equal; 
+        p_data_r = plot(X_r, Y_r, 'g.');
 
-        th = 0:pi/10000:2*pi;
-        xf1 = (R - delL) * cos(th) + x;
-        yf1 = (R - delL) * sin(th) + y;
+        p_fit = plot(xf1(th_lbidx:th_ubidx),yf1(th_lbidx:th_ubidx), 'k--');
+        p_cont = plot(xf1(th_lbidx),yf1(th_lbidx), 'bp');
+        plot(xf1(th_ubidx),yf1(th_ubidx), 'bp');
 
-        xf2 = (R + delL) * cos(th) + x;
-        yf2 = (R + delL) * sin(th) + y;
-
-        p_fit = plot(xf1,yf1, 'k--');
-        plot(xf2,yf2,'k--');
+        plot(xf2(th_lbidx:th_ubidx),yf2(th_lbidx:th_ubidx),'k--');
+        plot(xf2(th_lbidx),yf2(th_lbidx), 'bp');
+        plot(xf2(th_ubidx),yf2(th_ubidx), 'bp');
 
         xlabel('X'); ylabel('Y'); title('Circular Fitting');
-        legend([p_data, p_fit],'Original dataset', 'Optimized Circle')
+        legend([p_data_l, p_data_r, p_fit, p_cont],...
+               'Original dataset(Left Lane)', 'Original dataset(Right Lane)', ...
+               'Optimized Arcs', 'Arc Control Points')
     end
 
     %% Compute Fitting Error
@@ -107,6 +160,8 @@ function [x, y, R, delL, error, res] = CircleFitV2(X_l, X_r, Y_l, Y_r, w_l, w_r,
     error = struct();
     error.full_l = D_l - (R - delL); % Naive Error
     error.full_r = D_r - (R + delL);
+    error.max_l = max(error.full_l);
+    error.max_r = max(error.full_r);
 
     % Non-Weighted Error Computation 
     error.se_l = sum((error.full_l).^2);
